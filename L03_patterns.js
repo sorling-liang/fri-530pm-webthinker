99f4e337ce0d1ddb5864b3daf0b8dbe1fd6440b6
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-17 10:47:20 +0000

    Jul 17, 2026, 10:47 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -16,8 +16,10 @@ function draw() {
 
   // Task 1: Colour Gradient
   // let red = 0; // 0-255
+  let count = 1;
   for ( let red = 0; red < 256; red++ ) {
     fill(red, 0, 0);
+    circle(count*50,  circleY,  diameter);
   }
 
   // Task 2: Colour Loop
