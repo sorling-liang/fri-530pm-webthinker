643744b1493ba0796c07dd1cbb682cc002c3abbd
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-24 10:08:13 +0000

    Jul 24, 2026, 10:08 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -2,17 +2,17 @@
 function setup() {
     createCanvas(600,600);
     background("skyblue");
-    //noLoop(); // make draw() run once
+    noLoop(); // make draw() run once
 }
 
 
 function draw() {
-    // print(width);
+    print(width);
     // print(height);
     let xpos = 50;
     let ypos = 50;
     let ggColour = 0;
-    
+
     for (let count = 0; count < 5; count++) {
         fill(0, ggColour, 0);
         circle( xpos, ypos, 35);
