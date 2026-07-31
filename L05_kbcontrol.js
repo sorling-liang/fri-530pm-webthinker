1c92f05ef6d44e08ee04c5752a76c87fcd51bb9f
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-07-31 09:37:33 +0000

    Jul 31, 2026, 9:37 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,8 +1,15 @@
 // write your codes here
+let size = 5;
+
 function setup() {
     createCanvas(600, 600);
 }
 
-function draw() {
-    background("skyblue");
+// function draw() {
+//     background("skyblue");
+// }
+
+function mouseDragged() {
+    
+    size = size + 0.5;
 }
