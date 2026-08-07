88c7df0ca24302c2e23351e7577320b3e1f49686
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-07 10:17:39 +0000

    Aug 7, 2026, 10:17 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -17,28 +17,31 @@ function setup() {
     noStroke();
 }
 
+function draw() 
 
-function draw() {
-    background("lightblue");
-    fill("white");
-    rect(width/2, height/2, 300,300); // border, the bigger square
-    fill("black");
-    //     x     y    w   h
-    rect(xpos, ypos, 50, 50);
 
-    if (keyIsDown(UP_ARROW)) {
-        ypos = ypos -5;
-    }
-    else if (keyIsDown(DOWN_ARROW)) {
-        ypos = ypos +5;
-    }
-    else if (keyIsDown(LEFT_ARROW)) {
-        xpos = xpos -5;
-    }
-    else if (keyIsDown(RIGHT_ARROW)) {
-        xpos = xpos +5;
-    }
 
-    xpos = constrain(xpos, 75, 325);
-    ypos = constrain(ypos, 75, 325);
-}
+// function draw() {
+//     background("lightblue");
+//     fill("white");
+//     rect(width/2, height/2, 300,300); // border, the bigger square
+//     fill("black");
+//     //     x     y    w   h
+//     rect(xpos, ypos, 50, 50);
+
+//     if (keyIsDown(UP_ARROW)) {
+//         ypos = ypos -5;
+//     }
+//     else if (keyIsDown(DOWN_ARROW)) {
+//         ypos = ypos +5;
+//     }
+//     else if (keyIsDown(LEFT_ARROW)) {
+//         xpos = xpos -5;
+//     }
+//     else if (keyIsDown(RIGHT_ARROW)) {
+//         xpos = xpos +5;
+//     }
+
+//     xpos = constrain(xpos, 75, 325);
+//     ypos = constrain(ypos, 75, 325);
+// }
