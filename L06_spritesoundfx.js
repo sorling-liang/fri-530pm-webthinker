6f375c028046cbbf5d4624297c0867f798332b57
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-07 09:56:41 +0000

    Aug 7, 2026, 9:56 AM

--- L06_spritesoundfx.js
+++ L06_spritesoundfx.js
@@ -36,6 +36,6 @@ function draw() {
         xpos = xpos +1;
     }
 
-    xpos = constrain(xpos, 50, 350);
-    ypos = constrain(ypos, 50, 350);
+    xpos = constrain(xpos, 100, 300);
+    ypos = constrain(ypos, 50, 300);
 }
