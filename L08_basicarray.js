dcba609326ebbae5a87e1db687f606bccdaf8078
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-21 09:56:09 +0000

    Aug 21, 2026, 9:56 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -11,7 +11,7 @@ function draw() {
     //text("hello class", 10, 200);
     let ypos = 50;
     let foodList = ["burger", "tacos", "ice cream"];
-    for (let count=0; count<4; count++) {
+    for (let count=0; count<3; count++) {
         print( foodList[0] );
         text( foodList[0], 50, ypos);
         ypos = ypos + 50;
