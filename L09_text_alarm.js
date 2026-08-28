f728769913440f2ddbac393a173ac910bf417e17
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-28 10:31:33 +0000

    Aug 28, 2026, 10:31 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -7,7 +7,7 @@ function setup() {
     createCanvas(400,400);
     background(200);
 
-    intervalObject = setInterval(drawCircle, 1000); // 1000ms = 1 second
+    intervalObject = setInterval(drawCircle, 100); // 1000ms = 1 second
 }
 
 function draw() {
