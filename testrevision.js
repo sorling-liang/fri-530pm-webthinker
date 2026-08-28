82b81d2029e5abf221d099d66a89de6ef0785d87
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-08-28 10:44:58 +0000

    Aug 28, 2026, 10:44 AM

--- testrevision.js
+++ testrevision.js
@@ -1,9 +1,12 @@
 function setup() {
     createCanvas(500,500);
     background(200);
+    rect(0,0,)
 }
 
-function draw() {
-    fill("yellow");
-    triangle(50,100,  100,100,   75,50);
-}
+
+
+// function draw() {
+//     // fill("yellow");
+//     // triangle(50,100,  100,100,   75,50);
+// }
