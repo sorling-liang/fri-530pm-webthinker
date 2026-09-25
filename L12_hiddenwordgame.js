c9ae29329b824f720074f3f061cbaa331eef59dc
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-09-25 10:16:57 +0000

    Sep 25, 2026, 10:16 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -12,7 +12,7 @@ function setup() {
     guessInput.position(width/2, height/2+50);
 
     guessButton = createButton("Guess");
-    guessButton.position(width/2 + guessInput.width, height/2+25);
+    guessButton.position(width/2 + guessInput.width, height/2+53);
 }
 
 function draw() {
