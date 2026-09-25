538ed03cf2203bafb795498f24881441e87da129
LIANG Sor Ling <sorling.liang@computhink.com.sg>
2026-09-25 10:33:03 +0000

    Sep 25, 2026, 10:33 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -10,7 +10,7 @@ function setup() {
     // create canvas 800,700
     // color lightgray
 
-    myWordList = ["green", "black", "light", "watch"];
+    myWordList = ["green", "black", "light", "watch", "apple", "ground"];
 
     createCanvas(800, 700);
     background("lightgray");
